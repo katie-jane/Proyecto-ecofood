ee9b7bc5780884579581584ec4b1026ed7b66219
Jane Mendez Cervantes <jane.mendez.cervantes@gmail.com>
2020-09-19 16:51:17 -0500

    subiendo proyecto final codigo3

---
+++ js/main.js
@@ -0,0 +1,149 @@
+let carts=document.querySelectorAll('.add-cart');
+
+let products=[
+  {
+    name: 'Aceite de coco NATIVA',
+    tag: 'aceite-coco1n',
+    price: 69,
+    inCart:0
+  },
+  {
+    name: 'MCT oil NATIVA',
+    tag: 'aceite-MCT',
+    price: 170,
+    inCart:0
+  },
+  {
+    name: 'Mermelada de aguaymanto',
+    tag: 'aguay-merme',
+    price: 20,
+    inCart:0
+  },
+  {
+    name: 'Choc BITES',
+    tag: 'chocobytes',
+    price: 8,
+    inCart:0
+  }
+];
+
+for (let i=0; i<carts.length;i++){
+  carts[i].addEventListener('click', () =>{
+    cartNumbers(products[i]);
+    totalCost(products[i])
+  })
+}
+
+function onLoadCartNumbers(){
+  let productNumbers=localStorage.getItem('cartNumbers');
+
+  if(productNumbers){
+    document.querySelector('.cart span').textContent = productNumbers;
+  }
+}
+
+function cartNumbers(product){
+   
+  let productNumbers=localStorage.getItem('cartNumbers');
+ 
+  productNumbers=parseInt(productNumbers);
+  if(productNumbers){
+    localStorage.setItem('cartNumbers', productNumbers+1);
+    document.querySelector('.cart span').textContent=productNumbers + 1;
+  }else{
+    localStorage.setItem('cartNumbers',1);
+    document.querySelector('.cart span').textContent=1;
+  } 
+  setItems(product);
+}
+
+function setItems(product) {
+  let cartItems =localStorage.getItem("productsInCart");
+  cartItems=JSON.parse(cartItems);
+  
+  if(cartItems !=null) {
+    if(cartItems[product.tag] ==undefined){
+      cartItems={
+        ...cartItems,
+        [product.tag]:product
+      }
+    }
+    cartItems[product.tag].inCart +=1;
+
+  }else{
+    product.inCart=1;
+    cartItems = {
+    [product.tag]:product
+    }
+  
+  }
+  
+  localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+
+}
+function totalCost(product){
+  //console.log("The product price is", product.price);
+  let cartCost=localStorage.getItem('totalCost');
+  
+  console.log("MycartCost is", cartCost);
+  console.log(typeof cartCost);
+  
+ 
+   if (cartCost !=null) {
+      cartCost=parseInt(cartCost);
+      localStorage.setItem("totalCost", cartCost + product.price);
+   } else {
+     localStorage.setItem("totalCost", product.price)
+   }
+}
+   
+function displayCart(){
+  let cartItems=localStorage.getItem("productsInCart");
+  cartItems=JSON.parse(cartItems);
+  let productContainer=document.querySelector(".products");
+  let cartCost=localStorage.getItem('totalCost');
+
+  console.log(cartItems);
+
+  if (cartItems && productContainer) {
+    
+    productContainer.innerHTML='';
+    Object.values(cartItems).map(item =>{
+      productContainer.innerHTML +=  `
+      <div class="product">
+           <i class="fas fa-times-circle"></i>
+           <img src="./imagenes/${item.tag}.png">
+           <span>${item.name}</span>
+      </div> 
+      <div class="price">S/.${item.price},00</div>
+      <div class="quantity">
+          <i class="fas fa-caret-circle-left"></i>
+          <span>${item.inCart}</span>
+          <i class="fas fa-caret-circle-right"></i>
+      </div>
+      <div class="total">
+       S/.${item.inCart *item.price},00
+      </div>
+       `; 
+    });
+
+    productContainer.innerHTML += `
+        <div class="basketTotalContainer">
+            <h4 class="basketTotalTitle">
+                Total
+            </h4>
+            <h4 class="basketTotal">
+            S/.${cartCost},00
+            </h4>
+            
+                
+            
+    `
+
+
+      
+  }
+}
+
+onLoadCartNumbers();
+displayCart();
